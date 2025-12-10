6b7fe1a6c90ecabe4192c72076cc74519bbf60c8
kadegeist <sgeist@wpi.edu>
2025-12-10 13:29:00 -0500

    commit fix hotbar bug

--- Source/Loominescence/JournalRecipeEntry.cpp
+++ Source/Loominescence/JournalRecipeEntry.cpp
@@ -1,2 +1,1 @@
-#include "C:\codeing\senior\loominescense-2\Loominescence\Intermediate\Build\Win64\x64\LoominescenceEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_3.h"
 #include "JournalRecipeEntry.h"
